a0efe8f4a7c4b06968ffc74d338c2da8ee2beca9
doguhanniltextra <doguhangithub@gmail.com>
2025-07-28 09:24:42 +0300

    feat: DTOs for ListEvent method

--- src/main/java/com/doguy/tickets/mappers/EventMapper.java
+++ src/main/java/com/doguy/tickets/mappers/EventMapper.java
@@ -4,7 +4,10 @@ import com.doguy.tickets.domain.CreateEventRequest;
 import com.doguy.tickets.domain.CreateTicketTypeRequest;
 import com.doguy.tickets.domain.dtos.CreateEventRequestDto;
 import com.doguy.tickets.domain.dtos.CreateEventResponseDto;
+import com.doguy.tickets.domain.dtos.ListEventResponseDto;
+import com.doguy.tickets.domain.dtos.ListEventTicketTypeResponseDto;
 import com.doguy.tickets.domain.entities.Event;
+import com.doguy.tickets.domain.entities.TicketType;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -13,4 +16,8 @@ public interface EventMapper {
     CreateTicketTypeRequest fromDto(CreateTicketTypeRequest dto);
     CreateEventRequest fromDto(CreateEventRequestDto dto);
     CreateEventResponseDto toDto(Event event);
+
+    ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+    ListEventResponseDto toListEventResponseDto(Event event);
 }
