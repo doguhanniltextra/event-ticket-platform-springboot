f787813e0cbbb1dfaf067275022c2c17236395d0
doguhanniltextra <doguhangithub@gmail.com>
2025-07-29 08:30:39 +0300

    fix(config): quick fix

--- src/main/java/com/doguy/tickets/config/SecurityConfig.java
+++ src/main/java/com/doguy/tickets/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize
 
-                                .requestMatchers(HttpMethod.GET, "/api/v1/published-events")
+                                .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**")
                                 .permitAll()
 
                                 // CATCH ALL
