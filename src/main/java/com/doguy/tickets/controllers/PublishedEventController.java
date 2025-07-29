b2a14cebf342635d03e968b49408f8de8d9a0ca3
doguhanniltextra <doguhangithub@gmail.com>
2025-07-29 08:15:54 +0300

    feat: updated listPublishedEvents method

--- src/main/java/com/doguy/tickets/controllers/PublishedEventController.java
+++ src/main/java/com/doguy/tickets/controllers/PublishedEventController.java
@@ -1,6 +1,7 @@
 package com.doguy.tickets.controllers;
 
 import com.doguy.tickets.domain.dtos.responses.ListEventPublishedResponseDto;
+import com.doguy.tickets.domain.entities.Event;
 import com.doguy.tickets.mappers.EventMapper;
 import com.doguy.tickets.services.EventService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +23,18 @@ public class PublishedEventController {
     private final EventMapper eventMapper;
 
     @GetMapping
-    public ResponseEntity<Page<ListEventPublishedResponseDto>> listPublishedEvents(Pageable pageable) {
-        return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
+    public ResponseEntity<Page<ListEventPublishedResponseDto>> listPublishedEvents(@RequestParam(required = false) String q, Pageable pageable) {
+
+        Page<Event> events;
+
+        if(null != q && !q.trim().isEmpty()) {
+            events = eventService.searchPublishedEvents(q, pageable);
+        } else {
+            events = eventService.listPublishedEvents(pageable);
+        }
+
+
+        return ResponseEntity.ok(events
                 .map(eventMapper::toListEventPublishedResponseDto));
     }
 }
